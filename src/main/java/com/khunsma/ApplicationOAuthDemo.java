eb5a17c0cd7dd1aed8f5e37c4b8762c9b164a500
Khun Soe Moe Aung <khunsoemoe.aung@kbzbank.com>
2020-02-09 00:12:18 +0630

    Initialize Project

---
+++ src/main/java/com/khunsma/ApplicationOAuthDemo.java
@@ -0,0 +1,13 @@
+package com.khunsma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//https://howtodoinjava.com/spring-boot2/oauth2-auth-server/
+
+@SpringBootApplication
+public class ApplicationOAuthDemo {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationOAuthDemo.class, args);
+    }
+}
